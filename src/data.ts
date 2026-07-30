e6f4338d0b17f6975384a5d60564da18f47696b1
joao.xavier <joaovitor55670@gmail.com>
2026-07-30 17:41:18 -0300

    feat: add new project

--- src/data.ts
+++ src/data.ts
@@ -3,7 +3,7 @@ export const data = {
     "React",
     "TypeScript",
     "Tailwind CSS",
-    "CSS Modules",
+    "SCSS",
     "Node.js",
     "Next.js",
     "VTEX",
@@ -15,6 +15,13 @@ export const data = {
       image: "image/",
       technologies: ["React", "TypeScript", "Next.js"],
       link: "https://github.com/joaovxsantos/ecommerce-front",
+    },
+    {
+      title: "Design System Mars",
+      description: "Um Design System com 10 componentes acessíveis",
+      image: "image/",
+      technologies: ["React", "TypeScript", "Next.js", "SCSS"],
+      link: "https://github.com/joaovxsantos/design-system-mars",
     },
     {
       title: "Dashboard Front",
@@ -36,7 +43,7 @@ export const data = {
       data: "11/2024 - Até o momento",
       title: "Desenvolvedor Junior - DBC Company",
       description:
-        "Alocado no cliente Usaflex, atuo como desenvolvedor front-end utilizando React com TypeScript, aplicando boas práticas de componentização e tipagem forte. Também utlizo CSS Modules, garantindo organização e escopo de estilos, além de trabalhar no ecossistema da VTEX IO, desenvolvendo e personalizando lojas virtuais, criando componentes reutilizáveis e ajustando funcionalidades para atender demandas de e-commerce.",
+        "Alocado no cliente Usaflex, atuo como desenvolvedor front-end utilizando React com TypeScript, aplicando boas práticas de componentização e tipagem forte. Também utlizo SCSS, garantindo organização e escopo de estilos, além de trabalhar no ecossistema da VTEX IO, desenvolvendo e personalizando lojas virtuais, criando componentes reutilizáveis e ajustando funcionalidades para atender demandas de e-commerce.",
     },
     {
       data: "05/2024 - 11/2024",
@@ -57,7 +64,7 @@ export const data = {
     "Node.js": "#339933",
     VTEX: "#FFFFFF",
     "Tailwind CSS": "#FFFFFF",
-    "CSS Modules": "#2B4467",
+    SCSS: "#CC6699",
     "Next.js": "#e5e7eb",
   },
   bg: {
@@ -66,7 +73,7 @@ export const data = {
     "Node.js": "#000000",
     VTEX: "#7A1A9E",
     "Tailwind CSS": "#06B6D4",
-    "CSS Modules": "#FFFFFF",
+    SCSS: "#FFFFFF",
     "Next.js": "#374151",
   },
 };
